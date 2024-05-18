9eb79455cfe7ae24c4b2475e42dac61f66920d44
SebaGithubAP <150785157+SebaGithubAP@users.noreply.github.com>
2024-05-18 16:36:44 +0200

    Clean up, added comments, removed log in page...

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var map = L.map('map').setView([48.23009, 6.41616], 15);
   
+    // Copyright
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
